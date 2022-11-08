67cfb0741df19df2e7c7c043f8068dfa26590b9c
Alberto Celestino Ortiz Silva <alfaortiz.sis@gmail.com>
2022-11-08 15:08:27 -0400

    Segundo cambio al proyecto

--- main.js
+++ main.js
@@ -3,12 +3,20 @@ const $ = (selector) => document.querySelector(selector);
 const menuEmail = $('.navbar-email');
 const desktopMenu = $('.desktop-menu');
 
+const menuIconHam = $('.menu');
+const mobileMenu = $('.mobile-menu');
+
 // const menuEmail = document.querySelector('.navbar-email');
 // const desktopMenu = document.querySelector('.desktop-menu');
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
+menuIconHam.addEventListener("click", toggleMobileMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
-}
+};
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
+};
 
